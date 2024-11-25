58496aa1406c32332aea27778c0a8774ed71802d
Thibaut Jacqmin <ThibautJacqmin@users.noreply.github.com>
2024-11-25 16:06:38 +0100

    Solve issue in klayout vertices that can be non integers

--- +Utilities/get_vertices_from_klayout.m
+++ +Utilities/get_vertices_from_klayout.m
@@ -7,5 +7,17 @@ s = string(python_polygon.to_s);
 s = s.extractAfter("(");
 s = s.extractBefore(")");
 s = s.split(";");
+% Find mistakes in polygons after region operations like
+% 2367, 0/1872983, 7683 (three components, keep only the 2 first ones
+% and floatize rational number
+indices = find(s.contains("/"));
+if ~isempty(indices)
+    for i=indices'
+        t = s(i).split(",");
+        t = t(1:2, 1);
+        t(2) = floor(eval(t(2)));
+        s(i) = t.join(",");
+    end
+end
 s = s.split(",");
 matlab_vertices = str2double(s);
